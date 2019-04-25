afe5af7ed5f0d0fa042970dc569d5f3fea1b69e6
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-04-25 17:53:38 +0900

    test for similarity func

---
+++ test_knn.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import argparse
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from knn import calculate_similar_vector
+from collections import Counter
+
+iris = load_iris()
+X = pd.DataFrame(iris.data, columns=["a", "b", "c", "d"])
+Y = iris.target
+
+# import pdb; pdb.set_trace()
+train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.3, random_state=0)
+train_X = train_X.reset_index()
+test_X = test_X.reset_index()
+
+def test_calculate_similar_vector_shape():
+    idx = np.random.choice(test_X["index"].values)
+    vector = test_X[test_X["index"] == idx].values[0, 1:]
+    K = 10
+    assert(calculate_similar_vector(vector, train_X, train_Y, K).shape == (K, ))
+
+def test_calculate_similar_vector_value1():
+    idx = np.random.choice(test_X["index"].values)
+    vector = test_X[test_X["index"] == idx].values[0, 1:]
+    K = 10
+    count_arrays = calculate_similar_vector(vector, train_X, train_Y, K)
+    uniques, counts = np.unique(count_arrays, return_counts=True)
+    for i, (key_, value_) in enumerate(zip(uniques, counts)):
+        if i == 0:
+            max_key = key_
+            max_num = value_
+        else:
+            if max_num <= value_:
+                max_key = key_
+                max_num = value_
+
+    assert(max_key == test_Y[test_X["index"] == idx][0])
+    
+def test_calculate_similar_vector_value2():
+    idx = np.random.choice(test_X["index"].values)
+    vector = test_X[test_X["index"] == idx].values[0, 1:]
+    K = 10
+    count_arrays = calculate_similar_vector(vector, train_X, train_Y, K)
+    uniques, counts = np.unique(count_arrays, return_counts=True)
+    for i, (key_, value_) in enumerate(zip(uniques, counts)):
+        if i == 0:
+            max_key = key_
+            max_num = value_
+        else:
+            if max_num <= value_:
+                max_key = key_
+                max_num = value_
+
+    assert(max_key == test_Y[test_X["index"] == idx][0])
+
+if __name__ == '__main__':
+    test_calculate_similar_vector_value1()
