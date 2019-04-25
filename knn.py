76e7b73ab8dde0569f312a6f52ace84d27679432
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-04-25 18:10:54 +0900

    main

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import argparse
 from sklearn.datasets import load_iris
 from collections import Counter
+from sklearn.model_selection import train_test_split
 
 """
 input: 
@@ -31,13 +32,25 @@ def main(train_X: pd.DataFrame, test_X: pd.DataFrame, train_Y: np.ndarray, K: in
     test_size = test_X.shape[0]
     train_X = train_X.reset_index()
     test_X = test_X.reset_index()
-    each_sim_df = pd.DataFrame(columns=["node"])
-    for i in range(1, K + 1):
-        each_sim_df["label{}".format(i)] = 0
+    # each_sim_df = pd.DataFrame(columns=["node"])
+    each_sim_lists = []
     for i in range(test_size):
-        node = test_X.index[i]
-        vector = test_X.iloc[i, :].values
-        each_sim_df[node] = calculate_similar_vector(vector, train_X, train_Y, K)
+        node = test_X["index"].iloc[i]
+        vector = test_X[test_X["index"] == node].values[0, 1:]
+        count_arrays = calculate_similar_vector(vector, train_X, train_Y, K)
+        uniques, counts = np.unique(count_arrays, return_counts=True)
+        for i, (key_, value_) in enumerate(zip(uniques, counts)):
+            if i == 0:
+                max_key = key_
+                max_num = value_
+            else:
+                if max_num <= value_:
+                    max_key = key_
+                    max_num = value_
+        each_sim_lists.append(max_key)
+        print("node: ", node, " label: ", max_key)
+    print(each_sim_lists)
+    return np.array(each_sim_lists)
 
 
 
@@ -48,8 +61,12 @@ if __name__ == '__main__':
     #                 help=)
     # args = parser.parse_args()
     iris = load_iris()
-    train_X = iris.data
-    test_X = iris.data
-    train_Y = iris.target
+    X = pd.DataFrame(iris.data, columns=["a", "b", "c", "d"])
+    Y = iris.target
+
+    # import pdb; pdb.set_trace()
+    train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.3, random_state=0)
+    train_X = train_X.reset_index()
+    test_X = test_X.reset_index()
     k = 1
     main(train_X, test_X, train_Y, k)
